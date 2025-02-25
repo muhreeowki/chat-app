58fe9bf0adcf2a7230926df8f1c2aa7a7e3229cd
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2025-02-25 15:44:56 +0300

    Added TODO items

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// TODO: ** Add the following features **
+// 1. Chat Rooms that users can join and leave freely (kinda like house party)
+// 2. Chat Commands to do various things within the chat room
+// 3. Add CLI Client
+// 3. User Authentication & Authorization
+//    - Handle guests and limiting guest usage
+
 func main() {
 	store, err := NewPostgresStore()
 	if err != nil {
